f3dd631d30bb7ad6d129f5c8bfce64d9fa770bf8
Lza-etc <emj688531@gmail.com>
2024-02-19 12:29:06 +0530

    added reactive danger message

--- TaskMgmt/src/app/dashboard/project/project.component.ts
+++ TaskMgmt/src/app/dashboard/project/project.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
-import { Router } from '@angular/router';
-import { Group } from '../../models/group';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Project } from '../../models/project';
 
 @Component({
   selector: 'app-project',
@@ -10,33 +9,27 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './project.component.scss',
 })
 export class ProjectComponent implements OnInit {
-  groupForm!: FormGroup;
-  groupList!: Group[];
-  selectedGroup!: number;
+  groupId!: string;
+  projectList!: Project[];
 
   constructor(
-    private restService: RestService,
-    private formBuilder: FormBuilder,
-    private router: Router
+    private route: ActivatedRoute,
+    private restService: RestService
   ) {}
-  ngOnInit(): void {
-    var groupId = localStorage.getItem('groupId');
-    this.restService.GetAll('groups/' + groupId + '/projects').subscribe(
-      (result) => {
-        console.log(result);
-        this.groupList = result as Group[];
-      },
-      (error) => {
-        console.error('Error:', error);
-      }
-    );
-    this.groupForm = this.formBuilder.group({
-      groupName: ['', Validators.required],
-    });
-  }
 
-  public onSubmit() {
-    localStorage.getItem('groupId');
-    this.router.navigateByUrl('/projects');
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.groupId = params['groupId'];
+      this.restService.GetAll('groups/' + this.groupId + '/projects').subscribe(
+        (result) => {
+          console.log(result);
+          this.projectList = result as Project[];
+        },
+        (error) => {
+          console.error('Error:', error);
+        }
+      );
+      console.log('Group ID:', this.groupId);
+    });
   }
 }
---
+++ TaskMgmt/src/app/models/project.ts
@@ -0,0 +1,7 @@
+export interface Project {
+    projectId:number;
+    groupId: number;
+    projectName: string;
+    projectDescription: string;
+    ownerId: number
+}
